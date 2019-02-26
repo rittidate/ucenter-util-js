3da38136c3c6108db61f6a16d8156ae4826807c0
Rittidate Kittisarathamma <rittidate.k@kbtg.tech>
2019-02-26 10:08:24 +0700

    fix bug

--- src/common/Paths.ts
+++ src/common/Paths.ts
@@ -1,14 +1,13 @@
-import * as Path from 'path';
-import * as Fs from 'fs';
-import * as Url from 'url';
+import * as path from 'path';
+import * as fs from 'fs';
 
 declare var module: any;
 
 
 // Make sure any symlinks in the project folder are resolved:
 // https://github.com/facebookincubator/create-react-app/issues/637
-const appDirectory = Fs.realpathSync(process.cwd());
-const resolveApp = relativePath => Path.resolve(appDirectory, relativePath);
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
 (module).exports = {
     appResources: resolveApp('src/resources'),
